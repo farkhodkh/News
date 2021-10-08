eee0a36c9093d07d4f5e44e6045e3a6f2d4edcfd
farkhod <986532qw@gmail.com>
2021-10-08 14:55:08 +0300

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Farkhod on 08.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
